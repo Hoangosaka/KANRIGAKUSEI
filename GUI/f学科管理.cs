9b132f5bb6601acb591804cff7c70bd4cef485b8
Tanaka kenjirou <nguyentranhoang24@gmail.com>
2024-11-21 13:02:43 +0900

    学生管理フォームを作った

---
+++ GUI/f学科管理.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KANRIGAKUSEI.GUI
+{
+    public partial class f学科管理 : Form
+    {
+        public f学科管理()
+        {
+            InitializeComponent();
+        }
+    }
+}
